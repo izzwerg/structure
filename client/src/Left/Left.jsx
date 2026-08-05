9cdca69b1210eaccf0aa52480ed86d8f24acfbf3
Oleksandr Horobiy <izzwerg.kr@gmail.com>
2026-08-05 16:25:47 +0300

    done 4 today

--- client/src/Left/Left.jsx
+++ client/src/Left/Left.jsx
@@ -10,24 +10,28 @@ function Left({ isNavOpen, setIsNavOpen, page, setPage, handleLogout }) {
 
     return (
         <div className={clsx('left_container', { 'nav_open': isNavOpen, 'nav_closed': !isNavOpen })}>
-            <div className='nav_switcher pointer'
-                onClick={() => handleNavClick()}
-            >
-                <img src="menu.svg" alt="menu" className={clsx('switcher_logo', { 'nav_open': isNavOpen, 'nav_closed': !isNavOpen })} />
+            <div className='nav_buttons_container'>
+                <div className='nav_switcher pointer'
+                    onClick={() => handleNavClick()}
+                >
+                    <img src="menu.svg" alt="menu" className={clsx('switcher_logo', { 'nav_open': isNavOpen, 'nav_closed': !isNavOpen })} />
+                </div>
+                <div className={clsx('nav_btn pointer', { 'active': page === 'tree' })} onClick={() => setPage('tree')}>
+                    <img src="tree.svg" alt="tree" className='nav_btn_logo' />
+                    <span className='nav_btn_text'>TREE</span>
+                </div>
+                <div className={clsx('nav_btn pointer', { 'active': page === 'table' })} onClick={() => setPage('table')}>
+                    <img src="table.svg" alt="table" className='nav_btn_logo' />
+                    <span className='nav_btn_text'>TABLE</span>
+                </div>
+                <div className={clsx('nav_btn pointer', { 'active': page === 'person' })} onClick={() => setPage('person')}>
+                    <img src="person.svg" alt="person" className='nav_btn_logo' />
+                    <span className='nav_btn_text'>PERSON</span>
+                </div>
             </div>
-            <div className={clsx('nav_btn pointer', { 'active': page === 'tree' })} onClick={() => setPage('tree')}>
-                <img src="tree.svg" alt="tree" className='nav_btn_logo' />
-                <span className='nav_btn_text'>TREE</span>
-            </div>
-            <div className={clsx('nav_btn pointer', { 'active': page === 'table' })} onClick={() => setPage('table')}>
-                <img src="table.svg" alt="table" className='nav_btn_logo' />
-                <span className='nav_btn_text'>TABLE</span>
-            </div>
-            <div className={clsx('nav_btn pointer', { 'active': page === 'person' })} onClick={() => setPage('person')}>
-                <img src="person.svg" alt="person" className='nav_btn_logo' />
-                <span className='nav_btn_text'>PERSON</span>
-            </div>
-            <button onClick={handleLogout}>Вийти</button>
+            <button onClick={handleLogout} className='logout_btn pointer'>
+                <img src="mono-logout.svg" alt="person" className='nav_btn_logo' />
+            </button>
         </div>
     )
 }
