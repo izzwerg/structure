9fad426ccec7767d27d58eace5012a1cb1bac832
Oleksandr Horobiy <izzwerg.kr@gmail.com>
2026-08-15 16:50:53 +0300

    Update Tree.jsx

--- client/src/Right/Tree/Tree.jsx
+++ client/src/Right/Tree/Tree.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Modal from '@mui/material/Modal';
 import SubdivisionNode from './SubdivisionNode';
 import PositionNode from './PositionNode';
