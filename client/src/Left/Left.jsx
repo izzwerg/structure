79ac47a10b1c446b418578e5377495246262ed23
Oleksandr Horobiy <izzwerg.kr@gmail.com>
2026-08-19 15:28:55 +0300

    Update Left.jsx

--- client/src/Left/Left.jsx
+++ client/src/Left/Left.jsx
@@ -21,26 +21,26 @@ function Left({ isNavOpen, setIsNavOpen, page, setPage, handleLogout }) {
                 </div>
                 <div className={clsx('nav_btn pointer', { 'active': page === 'tree' })} onClick={() => setPage('tree')}>
                     <img src="tree.svg" alt="tree" className='nav_btn_logo' />
-                    <span className='nav_btn_text'>TREE</span>
+                    <span className='nav_btn_text'>СТРУКТУРА</span>
                 </div>
                 <div className={clsx('nav_btn pointer', { 'active': page === 'table' })} onClick={() => setPage('table')}>
                     <img src="table.svg" alt="table" className='nav_btn_logo' />
-                    <span className='nav_btn_text'>TABLE</span>
+                    <span className='nav_btn_text'>ТАБЛИЦЯ</span>
                 </div>
                 <div className={clsx('nav_btn pointer', { 'active': page === 'person' })} onClick={() => setPage('person')}>
                     <img src="person.svg" alt="person" className='nav_btn_logo' />
-                    <span className='nav_btn_text'>PERSON</span>
+                    <span className='nav_btn_text'>КАРТКИ</span>
                 </div>
 
                 <div className={clsx('nav_btn pointer', { 'active': page === 'person_archive' })} onClick={() => setPage('person_archive')}>
                     <img src="person.svg" alt="person" className='nav_btn_logo' />
-                    <span className='nav_btn_text'>ARCHIVE</span>
+                    <span className='nav_btn_text'>АРХІВ</span>
                 </div>
             </div>
             <div className='nav_buttons_container'>
                 {canMod && <div className={clsx('nav_btn pointer', { 'active': page === 'person_model' })} onClick={() => setPage('person_model')}>
                     <img src="gear.svg" alt="person" className='nav_btn_logo' />
-                    <span className='nav_btn_text'>MODEL</span>
+                    <span className='nav_btn_text'>НАЛАШТУВАННЯ</span>
                 </div>}
                 <button onClick={handleLogout} className='logout_btn pointer'>
                     <img src="mono-logout.svg" alt="person" className='nav_btn_logo' />
